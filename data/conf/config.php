b7364d63e86c859b22b95703b6e4ee6f148635e9
stoneworld <1113241948@qq.com>
2015-03-23 18:54:39 +0800

    第三方

--- data/conf/config.php
+++ data/conf/config.php
@@ -1,4 +1,5 @@
-<?php	return array (
+<?php	
+return array (
   'SP_DEFAULT_THEME' => 'simplebootx',
   'DEFAULT_THEME' => NULL,
   'SP_ADMIN_STYLE' => NULL,
@@ -8,4 +9,16 @@
   'COMMENT_NEED_CHECK' => 0,
   'COMMENT_TIME_INTERVAL' => 0,
   'MOBILE_TPL_ENABLED' => 0,
+	'THINK_SDK_QQ' =>
+	array (
+			'APP_KEY' => '101201968',
+			'APP_SECRET' => '3e3215c3b35b1dfa4fdd1a7c5cab968f',
+			'CALLBACK' => 'http://www.duostec.com/luokeke/Api/oauth/callback/type/qq',
+	),
+	'THINK_SDK_SINA' =>
+	array (
+			'APP_KEY' => '946092280',
+			'APP_SECRET' => 'b75e7b0416606a10fc549386bc773228',
+			'CALLBACK' => 'http://www.duostec.com/luokeke/Api/oauth/callback/type/sina',
+	),
 );?>

3e31ab7bdd6a28de2d4e2c2fe9a8a1bf96e4c50c
stoneworld <1113241948@qq.com>
2015-03-30 13:08:36 +0800

    推荐 婚纱礼服列表

--- application/Portal/Controller/PLastestController.class.php
+++ application/Portal/Controller/PLastestController.class.php
@@ -1,7 +1,7 @@
 <?php
 namespace Portal\Controller;
 use Common\Controller\HomeBaseController;
-class PLastestController extends HomeBaseController {
+class PLastestController extends IndexController {
 	
 	public function nav_index(){
 		$m 			= M('plastest');
@@ -10,12 +10,25 @@ class PLastestController extends HomeBaseController {
 				"name"	=> "最新推荐客片",
 				"items" => array(
 						0 =>array(	"label" => "{$msg['post_title']}",
-									"href" => U("Portal/PLastest/cid/{$msg['id']}")
+									"href" => U("Portal/PLastest/index/id/{$msg['id']}")
 						),
 				),
 		
 		
 		);
 		exit(json_encode($nav_arr));
+	}
+	
+	public function index(){
+		$where		= "recommended=1 and status=1";
+		$model_lastest = D('plastest');
+		$info		= $model_lastest->where($where)->limit(0,1)->find();
+		$this->assign("desc_beautiful",$this->_getAd("desc_beautiful"));
+		$this->assign("servePromise",$this->_getAd("servePromise"));
+		$this->assign("second_page_3",$this->_getAd("second_page_3"));
+		$this->assign("info",$info);
+		$smeta = json_decode($info['smeta'],true);
+		$this->assign("smeta",$smeta['photo']);
+		$this->display('/PCustom/new');
 	}
 }

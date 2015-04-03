92bed14c719ae48b58cfbf6c72ecf39ded755bdd
wangchengyu <635861193@qq.com>
2015-04-03 16:53:18 +0800

    第三方登录，配置文件优化

--- data/conf/config.php
+++ data/conf/config.php
@@ -13,12 +13,12 @@ return array (
 	array (
 			'APP_KEY' => '101201968',
 			'APP_SECRET' => '3e3215c3b35b1dfa4fdd1a7c5cab968f',
-			'CALLBACK' => 'http://www.duostec.com/luokeke/Api/oauth/callback/type/qq',
+			'CALLBACK' => 'http://'.$_SERVER['HTTP_HOST'].__ROOT__."/Api/oauth/callback/type/qq",
 	),
 	'THINK_SDK_SINA' =>
 	array (
 			'APP_KEY' => '946092280',
 			'APP_SECRET' => 'b75e7b0416606a10fc549386bc773228',
-			'CALLBACK' => 'http://www.duostec.com/luokeke/Api/oauth/callback/type/sina',
+			'CALLBACK' => 'http://'.$_SERVER['HTTP_HOST'].__ROOT__.'/Api/oauth/callback/type/sina',
 	),
 );?>

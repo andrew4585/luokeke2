e8b0fbadfa0bb05d26825572f1059387c3dd1936
stoneworld <1113241948@qq.com>
2015-06-03 18:54:47 +0800

    no message

--- simplewind/Core/Library/Think/WX/ThinkWechat.class.php
+++ simplewind/Core/Library/Think/WX/ThinkWechat.class.php
@@ -508,7 +508,7 @@ class ThinkWechat {
 					);
 			$data=json_encode($data);
 			$data=urldecode($data);
-			$restr = http_post ($url, $data);
+			$restr = http ( $url, $data, 'POST', array ( "Content-type: text/html; charset=utf-8" ), true );
 			Think\Log::record($restr,'WARN');
 			$restr = json_decode($restr,true);
 			//Think\Log::record($restr,'WARN');

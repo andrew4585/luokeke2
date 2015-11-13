eefe07a83856db1a527ac32e575f28893a7aa0f9
wangcy <635861193@qq.com>
2015-11-13 17:16:34 +0800

    no message

--- statics/phone/home/js/common.js
+++ statics/phone/home/js/common.js
@@ -1,3 +1,18 @@
 function redirect(url){
 	location.href=url;
 }
+
+function layer_alert(msg,url){
+	layer.open({
+	    content: msg,
+	    btn: ['OK'],
+	    shadeClose: false,
+	    yes: function(index){
+	    	if(url){
+	    		location.href=url
+	    	}else{
+		    	layer.close(index);
+	    	}
+	    }
+	});
+}
